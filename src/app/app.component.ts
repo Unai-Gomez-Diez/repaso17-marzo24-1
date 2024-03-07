482bb638f4072ae4b4d2957fc17cdd35e54f9c17
Unai-Gomez-Diez <unaigomez0807@gmail.com>
2024-03-07 14:57:36 +0100

    1 y 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Trabajador} from "./trabajador";
+import {FormBuilder} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'untitled1';
+export class AppComponent implements OnInit{
+  trabajador1: Trabajador = new Trabajador
+  horasTotales: number = 0
+  trabajadores: Trabajador[] = []
+
+  constructor(private formulario: FormBuilder) {
+  }
+  formTrabajador = this.formulario.group({
+    nombre:[''],
+    apellidos:[''],
+    sueldo:[''],
+    edad:[''],
+    horasExtras:[''],
+    sueldoAnual:['']
+  })
+
+  ngOnInit(): void {
+    for(let i=0;i<this.trabajador1.horasExtras.length;i++){
+      this.horasTotales = this.horasTotales + this.trabajador1.horasExtras[i]
+
+    }
+    this.trabajador1.sueldoAnual = this.horasTotales + this.trabajador1.sueldo
+    this.trabajadores.push(this.trabajador1)
+  }
+  submit(){
+    const nuevoTrabajador: Trabajador = {
+      nombre: this.formTrabajador.get('nombre')?.value || '', // Asigna una cadena vacía si el valor es nulo o undefined
+      apellidos: this.formTrabajador.get('apellidos')?.value || '',
+      sueldo: parseFloat(this.formTrabajador.get('sueldo')?.value || ''),
+      edad: '', // Ajusta según tus necesidades
+      horasExtras: [], // Ajusta según tus necesidades
+      sueldoAnual: 0 // Ajusta según tus necesidades
+    };
+
+    this.trabajadores.push(nuevoTrabajador)
+    this.formTrabajador.reset()
+  }
+
+
+
+  protected readonly parseInt = parseInt;
 }
---
+++ src/app/trabajador.ts
@@ -0,0 +1,8 @@
+export class Trabajador{
+  nombre: string = "Unai"
+  apellidos: string="Gómez Díez"
+  edad: string="19"
+  sueldo: number=200.0
+  horasExtras: number[]=[2,4,9,3,7,5]
+  sueldoAnual: number = 0
+}
